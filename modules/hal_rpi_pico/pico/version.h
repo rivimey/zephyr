65416c4fb3072ab1992d551d85ff39db76da3199
Yonatan Schachter <yonatan.schachter@gmail.com>
2021-11-29 23:51:48 +0200

    soc: Added support for Raspberry Pi's RP2040 SoC
    Added basic support for the RP2040 SoC. Support
    includes booting and starting the kernel, on one
    core only.
    
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ modules/hal_rpi_pico/pico/version.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Yonatan Schachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _PICO_VERSION_H
+#define _PICO_VERSION_H
+
+#define PICO_SDK_VERSION_MAJOR 1
+#define PICO_SDK_VERSION_MINOR 3
+#define PICO_SDK_VERSION_REVISION 0
+#define PICO_SDK_VERSION_STRING "1.3.0"
+
+#endif /* _PICO_VERSION_H */
